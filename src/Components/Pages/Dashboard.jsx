325521c973323e2bece007f3872b9e5ae6fd6326
madhusudan-divanex <madhusudhandivanextechnologies@gmail.com>
2026-01-20 18:52:35 +0530

    custom change to unique id

--- src/Components/Pages/Dashboard.jsx
+++ src/Components/Pages/Dashboard.jsx
@@ -302,7 +302,7 @@ function Dashboard() {
                                     <img src={item?.patientId?.profileImage ? `${base_url}/${item?.patientId?.profileImage}` : "/table-avatar.jpg"} alt="" />
                                     <div className="admin-table-sub-details">
                                       <h6>{item?.patientId?.name} </h6>
-                                      <p>ID: {item?.patientId?.customId}</p>
+                                      <p>ID: {item?.patientId?.unique_id}</p>
                                     </div>
                                   </div>
                                 </div>
